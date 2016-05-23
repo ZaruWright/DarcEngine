c7c1bce16166076c000f8ba886bc8c2fec1d4d2b
ZaruWright <samuel_gse@hotmail.com>
2016-05-23 21:35:44 +0200

    WINDOWS: release and debug compilation

--- DarcEngine/wbuild.bat
+++ DarcEngine/wbuild.bat
@@ -2,6 +2,8 @@ IF NOT EXIST _build (mkdir _build)
 
 cd _build
 cmake ../ -G "Visual Studio 12"
-msbuild "Darc Engine.sln"
-msbuild "INSTALL.vcxproj"
+msbuild "Darc Engine.sln" /property:Configuration=Debug
+msbuild "INSTALL.vcxproj" /property:Configuration=Debug
+msbuild "Darc Engine.sln" /property:Configuration=Release
+msbuild "INSTALL.vcxproj" /property:Configuration=Release
 cd ..
--- wbuild.bat
+++ wbuild.bat
@@ -2,6 +2,8 @@ IF NOT EXIST _build (mkdir _build)
 
 cd _build
 cmake ../ -G "Visual Studio 12"
-msbuild "Game.sln"
-msbuild "INSTALL.vcxproj"
+msbuild "Game.sln" /property:Configuration=Debug
+msbuild "INSTALL.vcxproj" /property:Configuration=Debug
+msbuild "Game.sln" /property:Configuration=Release
+msbuild "INSTALL.vcxproj" /property:Configuration=Release
 cd ..
